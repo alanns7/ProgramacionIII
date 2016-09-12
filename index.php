a21c78a96a190bd16db12c2cbd9be985fd8eed0b
alanns7 <alann.s7@hotmail.com>
2016-09-12 19:40:04 -0300

    1er commit

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
+<html>
+<head>
+	<title>Estacionamiento</title>
+</head>
+<body>
+<div>
+	<form action="nexo.php" method="POST" id="FormIngreso">
+
+<input type="text" name="texto" /><br><br>
+<input type="submit" name = "entrar" value="Entrar" /><br><br>
+<input type="submit" name="salir" value="Salir" /><br><br>
+
+</form>
+</div>
+	
 <?php 
 
 
  ?>
+
+
+</body>
+</html>
---
+++ nexo.php
@@ -0,0 +1,9 @@
+<?php 
+
+if(isset($_POST['entrar']))//isset para saber si esta instanciado, si da null o no devuelve un bool (true or false)
+ {
+	echo $_POST['texto'];
+ }
+
+
+ ?>
